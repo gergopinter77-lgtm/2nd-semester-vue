d1fe27dbd6371776a725153e167b457a7780e35c
shngnskrn <dilgun01@easv365.dk>
2026-05-15 16:29:03 +0200

    Update EventsView.vue

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="px-20 grid grid-cols-12 gap-2.5 pt-20">
     <h1 class="col-start-1 col-span-9 text-6xl text-blue font-semibold font-[raleway]"><span class="text-orange">J</span>oin our events & put your <br> name out there!</h1>
+  </div>
 
+  <div class="px-20 mt-66 grid grid-cols-12 gap-2.5 rounded-2xl px-9 py-8.75 flex flex-col justify-between">
+    <h2 class="col-start-1 col-span-9 text-4xl font-bold text-blue font-[raleway]">EVENTS</h2>
 
   </div>
 </template>
