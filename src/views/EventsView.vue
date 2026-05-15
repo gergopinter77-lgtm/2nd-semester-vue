9adecb49e97a6c1f8fdb7414d679a031ade2b98b
shngnskrn <dilgun01@easv365.dk>
2026-05-15 16:16:05 +0200

    Update EventsView.vue

---
+++ src/views/EventsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="px-20 grid grid-cols-12 gap-2.5 pt-20">
+    <h1 class="col-start-1 col-span-9 text-6xl text-blue font-semibold font-[raleway]"><span class="text-orange">J</span>oin our events & put your <br> name out there!</h1>
+
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
