329c7d769e256e50a050e2c48af5e9d14857af5a
gergopinter77-lgtm <gergopinter77@gmail.com>
2026-05-24 23:13:03 +0200

    Testimonial mobile

--- src/components/TestimonialPart.vue
+++ src/components/TestimonialPart.vue
@@ -1,31 +1,31 @@
 <template>
     <div class="w-screen h-85 bg-blue ">
-        <div class="grid grid-cols-12 gap-2.5 px-20">
+        <div class="grid grid-cols-12 gap-2.5 px-20 max-md:px-8 max-sm:px-2.5">
             <div class="col-start-1 col-span-2 pt-11">
-                <img src="https://i.imgur.com/TmXKaUA.jpeg" alt="" class="h-44.75 rounded-full">
+                <img src="https://i.imgur.com/TmXKaUA.jpeg" alt="" class="lg:h-44.75 md:h-33.75 sm:h-22.75 lg:mt-0 mt-6 rounded-full">
             </div>
             <div class="col-start-3 col-span-12 pt-22.75 pl-8 cols-span-2">
                 <div class="flex flex-row">
-                    <h4 class="text-white font-[Raleway] text-xl w-85">
+                    <h4 class="text-white font-[Raleway] lg:text-xl md:text-base sm:text-sm text-sm w-85">
                         Marlon Olvezecky <span class="font-light">- Creative User</span>
                     </h4>
                     <div class="flex flex-row col-span-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="#ea791e" d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z"/></svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="#ea791e" d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z"/></svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="#ea791e" d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z"/></svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="#ea791e" d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z"/></svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="#ea791e" d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="lg:w-7.5 lg:h-7.5 w-4 h-4" width="30" height="30" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="#ea791e" d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="lg:w-7.5 lg:h-7.5 w-4 h-4" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="#ea791e" d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="lg:w-7.5 lg:h-7.5 w-4 h-4"viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="#ea791e" d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="lg:w-7.5 lg:h-7.5 w-4 h-4" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="#ea791e" d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L12 17.275z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="lg:w-7.5 lg:h-7.5 w-4 h-4" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="#ea791e" d="m5.825 21l1.625-7.025L2 9.25l7.2-.625L12 2l2.8 6.625l7.2.625l-5.45 4.725L18.175 21L1２ 17.２7５z"/></svg>
                     </div>
                 </div>
-                <h5 class="text-3xl text-white font-[raleway] italic mt-1">
+                <h5 class="text-lg lg:text-3xl md:text-2xl sm:text-xl  text-white font-[raleway] italic mt-1">
                     Lorem ipsum dolor sit amet!
                 </h5>
-                <p class="text-white font-[raleway] italic font-thin w-152.5 mt-1">
+                <p class="text-white font-[raleway] italic font-thin lg:w-152.5 md:w-120 sm:w-110 w-110  mt-1">
                     Lorem ipsum dolor sit amet! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
             </div>
             <div class="col-start-4 col-span-12 flex flex-row">
-                <h4 class="uppercase text-white font-[raleway] pt-8 font-bold text-3xl">Check out what other users have to say</h4>
+                <h4 class="uppercase text-white font-[raleway] pt-8 font-bold text-lg lg:text-3xl md:text-2xl sm:text-lg md:mt-0 mt-0.75">Check out what other users have to say</h4>
                     <div class="pl-5 pt-8">
                         <button
                             class="w-9 h-9 rounded-full bg-white flex items-center justify-center shadow-[0_4px_24px_rgba(0,0,0,0.35)] cursor-pointer border-0 outline-none"
