4c231209d5e7e0d9979b8f512d0be2049bd82559
shngnskrn <dilgun01@easv365.dk>
2026-05-25 14:15:45 +0200

    Linking

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,9 +1,19 @@
 <script setup>
 import EventsPage from '@/components/EventsPage.vue';
+import NewsLetterPart from '@/components/NewsLetterPart.vue';
+import TestimonialPart from '@/components/TestimonialPart.vue';
 </script>
 
 <template>
   <main>
-    <EventsPage />
+    <div class="px-20 max-md:px-8 max-sm:px-2.5">
+      <EventsPage />
+    </div>
+    <div class="my-20">
+      <NewsLetterPart />
+    </div>
+    <div class="my-20">
+      <TestimonialPart />
+    </div>
   </main>
 </template>
